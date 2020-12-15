4b9ef56f989a03d0fe26587a9498a11cd9b47254
hajoohyuck <wngur21c@gmail.com>
2020-12-15 17:37:21 +0900

    Update and rename README.md to testjh.java

---
+++ testjh.java
@@ -0,0 +1,14 @@
+
+public class testjh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST");
+    # hello-world
+        System.out.println("aaaa");
+        System.out.println("ATEST");
+        System.out.println("QATESTQAQAQAQA");
+	}
+
+}
+
